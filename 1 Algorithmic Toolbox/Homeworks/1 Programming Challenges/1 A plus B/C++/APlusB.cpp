2a3846daa7e450dc59c2061d34924be99350c923
Maxim Uvarov <uvarov.max@gmail.com>
2018-06-04 17:15:41 +0300

    Upd

---
+++ 1 Algorithmic Toolbox/Homeworks/1 Programming Challenges/1 A plus B/C++/APlusB.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+  int a(0), b(0), sum(0);
+  std::cin >> a;
+  std::cin >> b;
+  sum = a + b;
+  std::cout << sum << std::endl;
+  system("pause");
+  return 0;
+}
