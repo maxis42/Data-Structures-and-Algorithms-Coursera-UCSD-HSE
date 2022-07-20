71690cd8fad04afa476865227a309be88404cee0
Maxim Uvarov <uvarov.max@gmail.com>
2022-07-20 11:33:35 +0300

    course 3 week 5 done

--- 3 Algorithms on Graph/Homeworks/Week 5/1_connecting_points/connecting_points.py
+++ 3 Algorithms on Graph/Homeworks/Week 5/1_connecting_points/connecting_points.py
@@ -1,17 +1,66 @@
-#Uses python3
+# Uses python3
 import sys
-import math
+import heapq
+from math import sqrt
+
 
 def minimum_distance(x, y):
-    result = 0.
-    #write your code here
+    result = 0
+    n = len(x)
+
+    cost = [float("inf") for _ in range(n)]
+    cost[0] = 0
+
+    visited = [False for _ in range(n)]
+
+    pq = []
+    # (priority, ID)
+    start = (0, 0)
+    heapq.heappush(pq, start)
+
+    while pq:
+        w, cur = heapq.heappop(pq)
+
+        # Check whether the node was already visited.
+        # It allows to use "lazy" priority queue.
+        if not visited[cur]:
+            visited[cur] = True
+
+            result += w
+
+            for i in range(n):
+                if not visited[i]:
+                    dist = sqrt((x[i] - x[cur])**2 + (y[i] - y[cur])**2)
+                    if dist < cost[i]:
+                        cost[i] = dist
+                        # We do not update the priority (cost) of the vertex.
+                        # Instead, we push the same vertex with updated priority.
+                        # As the updated priority is smaller than the previous
+                        # one, the new entry will be processed first. And then
+                        # the vertex will be marked as visited.
+                        heapq.heappush(pq, (dist, i))
+
     return result
 
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+def run_algo():
+    data = list(map(int, sys.stdin.read().split()))
     n = data[0]
     x = data[1::2]
     y = data[2::2]
     print("{0:.9f}".format(minimum_distance(x, y)))
+
+
+def run_test():
+    x = [0, 0, 1, 1]
+    y = [0, 1, 0, 1]
+
+    x = [0, 0, 1, 3, 3]
+    y = [0, 2, 1, 0, 2]
+
+    print("{0:.9f}".format(minimum_distance(x, y)))
+
+
+if __name__ == "__main__":
+    run_algo()
+    # run_test()
--- 3 Algorithms on Graph/Homeworks/Week 5/2_clustering/clustering.py
+++ 3 Algorithms on Graph/Homeworks/Week 5/2_clustering/clustering.py
@@ -1,15 +1,81 @@
-#Uses python3
+# Uses python3
 import sys
-import math
+from math import sqrt
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+
+class Edge:
+    def __init__(self, start, end, p_start, p_end):
+        self.start = start
+        self.end = end
+        self.p_start = p_start
+        self.p_end = p_end
+
+        self.weight = sqrt((p_end.x - p_start.x)**2 + (p_end.y - p_start.y)**2)
+
+    def __str__(self):
+        return f"Edge {self.start}-{self.end} weight={self.weight:.2f}"
+
+
+class DisjointSet:
+    def __init__(self, n):
+        self.n = n
+        self.ds = [i for i in range(n)]
+        self.num_sets = self.n
+
+    def find(self, i):
+        while self.ds[i] != i:
+            prev = i
+            i = self.ds[i]
+            # path compression
+            self.ds[prev] = i
+        return i
+
+    def union(self, i, j):
+        i_parent = self.find(i)
+        j_parent = self.find(j)
+
+        if i_parent != j_parent:
+            self.ds[j_parent] = i_parent
+            self.num_sets -= 1
+
 
 def clustering(x, y, k):
-    #write your code here
-    return -1.
+    ans = float("inf")
+
+    n = len(x)
+
+    # create all edges with weights
+    edges = []
+    for i in range(n):
+        for j in range(i+1, n):
+            edges.append(Edge(i, j, Point(x[i], y[i]), Point(x[j], y[j])))
+
+    # sort edges by weight
+    edges = sorted(edges, key=lambda edge: edge.weight)
+
+    dj = DisjointSet(n)
+
+    for edge in edges:
+        if dj.find(edge.start) != dj.find(edge.end):
+            if dj.num_sets > k:
+                dj.union(edge.start, edge.end)
+            else:
+                ans = min(ans, edge.weight)
+
+    return ans
 
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+def run_algo():
+    data = list(map(int, sys.stdin.read().split()))
     n = data[0]
     data = data[1:]
     x = data[0:2 * n:2]
@@ -17,3 +83,15 @@ if __name__ == '__main__':
     data = data[2 * n:]
     k = data[0]
     print("{0:.9f}".format(clustering(x, y, k)))
+
+
+def run_test():
+    x = [7, 4, 5, 1, 2, 5, 3, 7, 2, 4, 6, 2]
+    y = [6, 3, 1, 7, 7, 7, 3, 8, 8, 4, 7, 6]
+    k = 3
+    print("{0:.9f}".format(clustering(x, y, k)))
+
+
+if __name__ == "__main__":
+    # run_test()
+    run_algo()
