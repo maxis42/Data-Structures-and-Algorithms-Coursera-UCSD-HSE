646e11c52b06baec8cf5289e29c07cb03c38c972
Maxim Uvarov <uvarov.max@gmail.com>
2022-08-17 23:43:33 +0300

    course 4 week 2 tasks 1&2 done

--- 4 Algorithms on Strings/Homeworks/Week 2/1_bwt/bwt.py
+++ 4 Algorithms on Strings/Homeworks/Week 2/1_bwt/bwt.py
@@ -1,9 +1,37 @@
 # python3
 import sys
 
-def BWT(text):
-    return ""
 
-if __name__ == '__main__':
+class BWT:
+    def __init__(self, text):
+        self.text = text
+        self.bwt = self._bwt(self.text)
+
+    @staticmethod
+    def _bwt(text):
+        matrix = [text]
+        for i in range(len(text) - 1):
+            text = text[-1] + text[:-1]
+            matrix.append(text)
+
+        matrix.sort()
+
+        res = "".join([s[-1] for s in matrix])
+        return res
+
+
+def run_test():
+    text = "ACACACAC$"
+    bwt = BWT(text)
+    print(bwt.bwt)
+
+
+def run_algo():
     text = sys.stdin.readline().strip()
-    print(BWT(text))
+    bwt = BWT(text)
+    print(bwt.bwt)
+
+
+if __name__ == "__main__":
+    # run_test()
+    run_algo()
--- 4 Algorithms on Strings/Homeworks/Week 2/2_bwtinverse/bwtinverse.py
+++ 4 Algorithms on Strings/Homeworks/Week 2/2_bwtinverse/bwtinverse.py
@@ -1,11 +1,65 @@
 # python3
 import sys
-
-def InverseBWT(bwt):
-    # write your code here
-    return ""
+from collections import defaultdict
 
 
-if __name__ == '__main__':
+class InverseBWT:
+    def __init__(self, bwt):
+        self.bwt = bwt
+        self.text = self._text(self.bwt)
+
+    @staticmethod
+    def _text(bwt):
+        last_col = bwt
+        first_col = "".join(sorted(bwt))
+
+        first_d = dict()
+        first_d_extra = []
+        first_cnt = 0
+        first_char = first_col[0]
+        last_d = dict()
+        last_d_extra = []
+        last_cnt = dict()
+        for i in range(len(bwt)):
+            if first_col[i] != first_char:
+                first_char = first_col[i]
+                first_cnt = 0
+
+            first_d[(first_char, first_cnt)] = i
+            first_d_extra.append((first_char, first_cnt))
+            first_cnt += 1
+
+            last_char = last_col[i]
+            if last_char not in last_cnt:
+                last_cnt[last_char] = 0
+
+            last_d[(last_char, last_cnt[last_char])] = i
+            last_d_extra.append((last_char, last_cnt[last_char]))
+            last_cnt[last_char] += 1
+
+        key = ("$", 0)
+        s = ""
+        for i in range(len(bwt)):
+            next_id = last_d[key]
+            next_char = first_col[next_id]
+            s += next_char
+            key = first_d_extra[next_id]
+
+        return s
+
+
+def run_test():
+    bwt = "SMNPBNNAAAAA$A"
+    ibwt = InverseBWT(bwt)
+    print(ibwt.text)
+
+
+def run_algo():
     bwt = sys.stdin.readline().strip()
-    print(InverseBWT(bwt))
+    ibwt = InverseBWT(bwt)
+    print(ibwt.text)
+
+
+if __name__ == "__main__":
+    # run_test()
+    run_algo()
