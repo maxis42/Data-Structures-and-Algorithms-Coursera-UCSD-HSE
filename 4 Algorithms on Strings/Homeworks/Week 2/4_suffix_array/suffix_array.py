48a26e73747b229f400e1042023df22d4d297a23
Maxim Uvarov <uvarov.max@gmail.com>
2022-08-18 14:09:46 +0300

    course 4 week 2 done

--- 4 Algorithms on Strings/Homeworks/Week 2/4_suffix_array/suffix_array.py
+++ 4 Algorithms on Strings/Homeworks/Week 2/4_suffix_array/suffix_array.py
@@ -3,18 +3,33 @@ import sys
 
 
 def build_suffix_array(text):
-  """
-  Build suffix array of the string text and
-  return a list result of the same length as the text
-  such that the value result[i] is the index (0-based)
-  in text where the i-th lexicographically smallest
-  suffix of text starts.
-  """
-  result = []
-  # Implement this function yourself
-  return result
+    """
+    Build suffix array of the string text and
+    return a list result of the same length as the text
+    such that the value result[i] is the index (0-based)
+    in text where the i-th lexicographically smallest
+    suffix of text starts.
+    """
+    data = []
+    for i in range(len(text)):
+        data.append((text[i:], i))
+
+    data.sort(key=lambda x: x[0])
+    result = [i for s, i in data]
+
+    return result
 
 
-if __name__ == '__main__':
-  text = sys.stdin.readline().strip()
-  print(" ".join(map(str, build_suffix_array(text))))
+def run_test():
+    text = "PANAMABANANAS$"
+    print(" ".join(map(str, build_suffix_array(text))))
+
+
+def run_algo():
+    text = sys.stdin.readline().strip()
+    print(" ".join(map(str, build_suffix_array(text))))
+
+
+if __name__ == "__main__":
+    # run_test()
+    run_algo()
