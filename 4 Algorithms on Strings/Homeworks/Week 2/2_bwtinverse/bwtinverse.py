4585ca0fcb1e970a46b8e8dff88d76fb7c934977
Maxim Uvarov <uvarov.max@gmail.com>
2022-08-18 08:36:36 +0300

    course 4 week 2 tasks 1&2 done

--- 4 Algorithms on Strings/Homeworks/Week 2/2_bwtinverse/bwtinverse.py
+++ 4 Algorithms on Strings/Homeworks/Week 2/2_bwtinverse/bwtinverse.py
@@ -13,37 +13,28 @@ class InverseBWT:
         last_col = bwt
         first_col = "".join(sorted(bwt))
 
-        first_d = dict()
-        first_d_extra = []
-        first_cnt = 0
-        first_char = first_col[0]
-        last_d = dict()
-        last_d_extra = []
-        last_cnt = dict()
+        first_char_to_cnt = []
+        first_counter = defaultdict(int)
+
+        last_char_to_cnt = []
+        last_counter = defaultdict(int)
+
         for i in range(len(bwt)):
-            if first_col[i] != first_char:
-                first_char = first_col[i]
-                first_cnt = 0
+            first_char_to_cnt.append(first_counter[first_col[i]])
+            first_counter[first_col[i]] += 1
 
-            first_d[(first_char, first_cnt)] = i
-            first_d_extra.append((first_char, first_cnt))
-            first_cnt += 1
+            last_char_to_cnt.append(last_counter[last_col[i]])
+            last_counter[last_col[i]] += 1
 
-            last_char = last_col[i]
-            if last_char not in last_cnt:
-                last_cnt[last_char] = 0
+        last_char_id_to_id = dict()
+        for i in range(len(bwt)):
+            last_char_id_to_id[(last_col[i], last_char_to_cnt[i])] = i
 
-            last_d[(last_char, last_cnt[last_char])] = i
-            last_d_extra.append((last_char, last_cnt[last_char]))
-            last_cnt[last_char] += 1
-
-        key = ("$", 0)
         s = ""
-        for i in range(len(bwt)):
-            next_id = last_d[key]
-            next_char = first_col[next_id]
-            s += next_char
-            key = first_d_extra[next_id]
+        i = 0
+        for _ in range(len(bwt)):
+            i = last_char_id_to_id[(first_col[i], first_char_to_cnt[i])]
+            s += first_col[i]
 
         return s
 
