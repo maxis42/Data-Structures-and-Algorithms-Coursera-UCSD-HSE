85766c74f18a0f47572833174c95c3cff9af086e
Maxim Uvarov <uvarov.max@gmail.com>
2022-09-02 10:46:09 +0300

    course 5 week 2 task 1 done

--- 5 Advanced Algorithms and Complexity/Homeworks/Week 2/1_energy_values/energy_values.py
+++ 5 Advanced Algorithms and Complexity/Homeworks/Week 2/1_energy_values/energy_values.py
@@ -1,74 +1,76 @@
 # python3
+class GaussMethod:
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.n = len(self.matrix)
 
-EPS = 1e-6
-PRECISION = 20
+    def solve(self):
+        # forward pass
+        for i in range(self.n):
+            # find row with the first nonzero column in i-th position
+            for k in range(i, self.n):
+                if self.matrix[k][i] != 0:
+                    cur = k
+                    break
+            else:
+                return "Multiple solutions!"
 
-class Equation:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+            # swap rows
+            if cur != i:
+                self.matrix[i], self.matrix[cur] = self.matrix[cur], self.matrix[i]
 
-class Position:
-    def __init__(self, column, row):
-        self.column = column
-        self.row = row
+            # make the first coefficient equals to 1
+            coeff = self.matrix[i][i]
+            for j in range(i, self.n + 1):
+                self.matrix[i][j] /= coeff
 
-def ReadEquation():
+            # subtract current row from the next rows in order to
+            # eliminate dependence from current variable
+            for k in range(i + 1, self.n):
+                coeff = self.matrix[k][i]
+                for j in range(i, self.n + 1):
+                    self.matrix[k][j] -= coeff * self.matrix[i][j]
+
+        # backward pass
+        for i in range(self.n - 1, -1, -1):
+            for j in range(i - 1, -1, -1):
+                coeff = self.matrix[j][i]
+                self.matrix[j][i] -= coeff * self.matrix[i][i]
+                self.matrix[j][self.n] -= coeff * self.matrix[i][self.n]
+
+        res = [self.matrix[i][-1] for i in range(self.n)]
+        return res
+
+    def print(self):
+        print("Matrix:")
+        for row in self.matrix:
+            print(row)
+        print()
+
+
+def run_test():
+    matrix = [
+        [5, -5, -1],
+        [-1, -2, -1],
+    ]
+
+    gm = GaussMethod(matrix)
+    res = gm.solve()
+    print(" ".join(map(lambda x: str(round(x, 4)), res)))
+
+
+def run_algo():
     size = int(input())
-    a = []
-    b = []
-    for row in range(size):
-        line = list(map(float, input().split()))
-        a.append(line[:size])
-        b.append(line[size])
-    return Equation(a, b)
+    matrix = []
+    for _ in range(size):
+        row = list(map(float, input().split()))
+        matrix.append(row)
 
-def SelectPivotElement(a, used_rows, used_columns):
-    # This algorithm selects the first free element.
-    # You'll need to improve it to pass the problem.
-    pivot_element = Position(0, 0)
-    while used_rows[pivot_element.row]:
-        pivot_element.row += 1
-    while used_columns[pivot_element.column]:
-        pivot_element.column += 1
-    return pivot_element
+    gm = GaussMethod(matrix)
+    res = gm.solve()
+    print(" ".join(map(lambda x: str(round(x, 4)), res)))
 
-def SwapLines(a, b, used_rows, pivot_element):
-    a[pivot_element.column], a[pivot_element.row] = a[pivot_element.row], a[pivot_element.column]
-    b[pivot_element.column], b[pivot_element.row] = b[pivot_element.row], b[pivot_element.column]
-    used_rows[pivot_element.column], used_rows[pivot_element.row] = used_rows[pivot_element.row], used_rows[pivot_element.column]
-    pivot_element.row = pivot_element.column;
-
-def ProcessPivotElement(a, b, pivot_element):
-    # Write your code here
-    pass
-
-def MarkPivotElementUsed(pivot_element, used_rows, used_columns):
-    used_rows[pivot_element.row] = True
-    used_columns[pivot_element.column] = True
-
-def SolveEquation(equation):
-    a = equation.a
-    b = equation.b
-    size = len(a)
-
-    used_columns = [False] * size
-    used_rows = [False] * size
-    for step in range(size):
-        pivot_element = SelectPivotElement(a, used_rows, used_columns)
-        SwapLines(a, b, used_rows, pivot_element)
-        ProcessPivotElement(a, b, pivot_element)
-        MarkPivotElementUsed(pivot_element, used_rows, used_columns)
-
-    return b
-
-def PrintColumn(column):
-    size = len(column)
-    for row in range(size):
-        print("%.20lf" % column[row])
 
 if __name__ == "__main__":
-    equation = ReadEquation()
-    solution = SolveEquation(equation)
-    PrintColumn(solution)
-    exit(0)
+    run_test()
+    # run_algo()
