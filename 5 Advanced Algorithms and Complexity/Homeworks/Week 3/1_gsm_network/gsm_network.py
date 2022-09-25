7129da8be11ea5766e13a575bcec832f2db66de7
Maxim Uvarov <uvarov.max@gmail.com>
2022-09-25 21:38:19 +0300

    course 5 week 3 tasks 1 & 2 done

--- 5 Advanced Algorithms and Complexity/Homeworks/Week 3/1_gsm_network/gsm_network.py
+++ 5 Advanced Algorithms and Complexity/Homeworks/Week 3/1_gsm_network/gsm_network.py
@@ -1,14 +1,79 @@
 # python3
-n, m = map(int, input().split())
-edges = [ list(map(int, input().split())) for i in range(m) ]
 
-# This solution prints a simple satisfiable formula
-# and passes about half of the tests.
-# Change this function to solve the problem.
-def printEquisatisfiableSatFormula():
-    print("3 2")
-    print("1 2 0")
-    print("-1 -2 0")
-    print("1 -2 0")
+class GraphColoringToSAT:
+    def __init__(self, num_vertices, edges):
+        self.num_vertices = num_vertices
+        self.edges = edges
 
-printEquisatisfiableSatFormula()
+    @staticmethod
+    def _convert_vertex_to_sat(v):
+        v_last_id = v * 3
+        v_ids = (v_last_id - 2, v_last_id - 1, v_last_id)
+
+        sat_vertex = [
+            # exactly one color for the first vertex
+            (v_ids[0], v_ids[1], v_ids[2]),
+            (-v_ids[0], -v_ids[1]),
+            (-v_ids[0], -v_ids[2]),
+            (-v_ids[1], -v_ids[2]),
+        ]
+        return sat_vertex
+
+    @staticmethod
+    def _convert_edge_to_sat(v1, v2):
+        v1_last_id = v1 * 3
+        v1_ids = (v1_last_id - 2, v1_last_id - 1, v1_last_id)
+
+        v2_last_id = v2 * 3
+        v2_ids = (v2_last_id - 2, v2_last_id - 1, v2_last_id)
+
+        sat_edge = [
+            # different colors on edge ends
+            (-v1_ids[0], -v2_ids[0]),
+            (-v1_ids[1], -v2_ids[1]),
+            (-v1_ids[2], -v2_ids[2]),
+        ]
+        return sat_edge
+
+    def convert(self):
+        sat = []
+
+        for i in range(self.num_vertices):
+            sat.extend(self._convert_vertex_to_sat(i + 1))
+
+        for v1, v2 in self.edges:
+            sat.extend(self._convert_edge_to_sat(v1, v2))
+        return sat
+
+
+def run_test():
+    num_vertices, num_edges = 3, 3
+    edges = [
+        (1, 2),
+        (2, 3),
+        (1, 3),
+    ]
+    gc_to_sat = GraphColoringToSAT(num_vertices, edges)
+    sat = gc_to_sat.convert()
+    print(len(sat), num_vertices * 3)
+    for clause in sat:
+        s = " ".join(map(str, clause))
+        s += " 0"
+        print(s)
+
+
+def run_algo():
+    num_vertices, num_edges = map(int, input().split())
+    edges = [list(map(int, input().split())) for i in range(num_edges)]
+    gc_to_sat = GraphColoringToSAT(num_vertices, edges)
+    sat = gc_to_sat.convert()
+    print(len(sat), num_vertices*3)
+    for clause in sat:
+        s = " ".join(map(str, clause))
+        s += " 0"
+        print(s)
+
+
+if __name__ == "__main__":
+    # run_test()
+    run_algo()
--- 5 Advanced Algorithms and Complexity/Homeworks/Week 3/2_cleaning_apartment/cleaning_apartment.py
+++ 5 Advanced Algorithms and Complexity/Homeworks/Week 3/2_cleaning_apartment/cleaning_apartment.py
@@ -1,14 +1,111 @@
 # python3
-n, m = map(int, input().split())
-edges = [ list(map(int, input().split())) for i in range(m) ]
+from itertools import combinations
+from collections import defaultdict
 
-# This solution prints a simple satisfiable formula
-# and passes about half of the tests.
-# Change this function to solve the problem.
-def printEquisatisfiableSatFormula():
-    print("3 2")
-    print("1 2 0")
-    print("-1 -2 0")
-    print("1 -2 0")
 
-printEquisatisfiableSatFormula()
+class HamiltonianPathToSAT:
+    def __init__(self, num_vertices, edges):
+        self.num_vertices = num_vertices
+        self.edges = edges
+
+    def _belongs_to_a_path(self):
+        sat = []
+        for v in range(1, self.num_vertices + 1):
+            clause = tuple(range(
+                (v - 1) * self.num_vertices + 1,
+                (v - 1) * self.num_vertices + self.num_vertices + 1
+            ))
+            sat.append(clause)
+        return sat
+
+    def _once_in_a_path(self):
+        sat = []
+        for v in range(1, self.num_vertices + 1):
+            for pos1 in range(1, self.num_vertices):
+                for pos2 in range(pos1 + 1, self.num_vertices + 1):
+                    x1 = -(self.num_vertices*(v - 1) + pos1)
+                    x2 = -(self.num_vertices*(v - 1) + pos2)
+                    sat.append((x1, x2))
+        return sat
+
+    def _each_pos_by_some_vertex(self):
+        sat = []
+        for pos in range(1, self.num_vertices + 1):
+            clause = tuple(range(pos, self.num_vertices**2 + 1, self.num_vertices))
+            sat.append(clause)
+        return sat
+
+    def _no_two_vertices_same_pos(self):
+        sat = []
+        for pos in range(1, self.num_vertices + 1):
+            for v1 in range(1, self.num_vertices):
+                for v2 in range(v1 + 1, self.num_vertices + 1):
+                    x1 = -(self.num_vertices*(v1 - 1) + pos)
+                    x2 = -(self.num_vertices*(v2 - 1) + pos)
+                    sat.append((x1, x2))
+        return sat
+
+    def _connected_by_an_edge(self):
+        adj_list = defaultdict(set)
+        for v1, v2 in self.edges:
+            adj_list[v1].add(v2)
+            adj_list[v2].add(v1)
+
+        sat = []
+        for v1 in range(1, self.num_vertices):
+            for v2 in range(v1 + 1, self.num_vertices + 1):
+                if v2 not in adj_list[v1]:
+                    for pos in range(1, self.num_vertices):
+                        # v1 to v2
+                        x1 = -((v1 - 1)*self.num_vertices + pos)
+                        x2 = -((v2 - 1)*self.num_vertices + (pos + 1) % (self.num_vertices + 1))
+                        sat.append((x1, x2))
+
+                        # v2 to v1
+                        x1 = -((v2 - 1) * self.num_vertices + pos)
+                        x2 = -((v1 - 1) * self.num_vertices + (pos + 1) % (self.num_vertices + 1))
+                        sat.append((x1, x2))
+        return sat
+
+    def convert(self):
+        sat = []
+        sat.extend(self._belongs_to_a_path())
+        sat.extend(self._once_in_a_path())
+        sat.extend(self._each_pos_by_some_vertex())
+        sat.extend(self._no_two_vertices_same_pos())
+        sat.extend(self._connected_by_an_edge())
+        return sat
+
+
+def run_test():
+    num_vertices, num_edges = 5, 4
+    edges = [
+        (1, 2),
+        (2, 3),
+        (5, 4),
+        (3, 5),
+    ]
+    hp_to_sat = HamiltonianPathToSAT(num_vertices, edges)
+    sat = hp_to_sat.convert()
+    print(len(sat), num_vertices**2)
+    for clause in sat:
+        s = " ".join(map(str, clause))
+        s += " 0"
+        print(s)
+
+
+def run_algo():
+    num_vertices, num_edges = map(int, input().split())
+    edges = [list(map(int, input().split())) for i in range(num_edges)]
+    hp_to_sat = HamiltonianPathToSAT(num_vertices, edges)
+    sat = hp_to_sat.convert()
+    print(len(sat), num_vertices**2)
+    for clause in sat:
+        s = " ".join(map(str, clause))
+        s += " 0"
+        print(s)
+
+
+if __name__ == "__main__":
+    # run_test()
+    run_algo()
