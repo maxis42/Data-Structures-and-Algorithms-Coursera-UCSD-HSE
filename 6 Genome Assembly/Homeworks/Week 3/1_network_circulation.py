5d22bf9762640d660abadc1be8e23d0284f0d90a
Maxim Uvarov <uvarov.max@gmail.com>
2022-11-11 15:57:00 +0200

    course 6 week 3 task 1 done

--- 6 Genome Assembly/Homeworks/Week 3/1_network_circulation.py
+++ 6 Genome Assembly/Homeworks/Week 3/1_network_circulation.py
@@ -1,9 +1,9 @@
 # python 3
-from copy import deepcopy
 from collections import deque
+from random import randint, choice
 
 
-class Edge:
+class EdgeFG:
     def __init__(self, start, end, capacity):
         self.start = start
         self.end = end
@@ -29,6 +29,8 @@ class FlowGraph:
     edge for a given edge quickly.
     """
 
+    INF = 10 ** 6
+
     def __init__(self, n):
         self.n = n
         # List of all - forward and backward - edges
@@ -42,15 +44,15 @@ class FlowGraph:
         # a backward edge, so all forward edges are stored
         # at even indices (starting from 0), whereas
         # backward edges are stored at odd indices.
-        forward_edge = Edge(start, end, capacity)
-        backward_edge = Edge(end, start, 0)
+        forward_edge = EdgeFG(start, end, capacity)
+        backward_edge = EdgeFG(end, start, 0)
         self.graph[start].append(len(self.edges))
         self.edges.append(forward_edge)
         self.graph[end].append(len(self.edges))
         self.edges.append(backward_edge)
 
     def find_shortest_path(self, start, end):
-        parent = [None] * self.n
+        prev_edge = [None] * self.n
 
         processed = [False] * self.n
         processed[start] = True
@@ -58,62 +60,53 @@ class FlowGraph:
         q = deque([start])
         while q:
             cur = q.popleft()
-
             if cur == end:
                 break
 
             for edge_id in self.graph[cur]:
                 edge = self.edges[edge_id]
-                if (not processed[edge.end]) and (edge.flow > 0):
+                if (not processed[edge.end]) and (edge.flow < edge.capacity):
                     q.append(edge.end)
-                    parent[edge.end] = (edge_id, edge)
+                    prev_edge[edge.end] = edge_id
                     processed[edge.end] = True
 
-        path = []
-        if parent[end] is not None:
-            next_node = end
-            while next_node != start:
-                edge_id, edge = parent[next_node]
-                path.append((edge_id, edge))
-                next_node = edge.start
+        path = None
+        if prev_edge[end] is not None:
+            path = []
+            node = end
+            while node != start:
+                edge_id = prev_edge[node]
+                path.append(edge_id)
+                node = self.edges[edge_id].start
 
             path = path[::-1]
         return path
 
-    def max_flow(self, start, end):
-        graph_r = deepcopy(self)
-        for i in range(len(graph_r.edges) // 2):
-            graph_r.edges[i * 2].flow = graph_r.edges[i * 2].capacity
-
-        flow = 0
+    def maximize_flow(self, start, end):
         while True:
             # find the shortest path in residuals graph
-            path = graph_r.find_shortest_path(start, end)
-            if not path:
+            path = self.find_shortest_path(start, end)
+            if path is None:
                 break
 
             # find min flow through the path
-            min_flow = path[0][1].flow
-            for _, edge in path[1:]:
-                min_flow = min(min_flow, edge.flow)
+            min_flow = FlowGraph.INF
+            for edge_id in path:
+                edge = self.edges[edge_id]
+                min_flow = min(min_flow, edge.capacity - edge.flow)
 
             # update graphs
-            for edge_id, _ in path:
+            for edge_id in path:
                 if edge_id % 2 == 0:
-                    self.edges[edge_id].flow += min_flow
-
-                    graph_r.edges[edge_id].flow -= min_flow
-                    graph_r.edges[edge_id + 1].flow += min_flow
+                    self.edges[edge_id].capacity -= min_flow
+                    self.edges[edge_id + 1].capacity += min_flow
                 else:
-                    self.edges[edge_id - 1].flow -= min_flow
+                    self.edges[edge_id - 1].capacity += min_flow
+                    self.edges[edge_id].capacity -= min_flow
 
-                    graph_r.edges[edge_id - 1].flow -= min_flow
-                    graph_r.edges[edge_id].flow += min_flow
-
-            # update flow
-            flow += min_flow
-
-        return flow
+        for i in range(len(self.edges) // 2):
+            self.edges[i * 2].flow = self.edges[i * 2 + 1].capacity
+            self.edges[i * 2].capacity += self.edges[i * 2 + 1].capacity
 
 
 class EdgeNC:
@@ -144,7 +137,10 @@ class NetworkCirculation:
 
     def solve(self):
         if self.check_naive_solution():
-            circulation = [edge.low_bound for edge in self.edges]
+            circulation = []
+            for i, edge in enumerate(self.edges):
+                edge.flow = edge.low_bound
+                circulation.append(edge.flow)
             return circulation
 
         adj_list_prev = [[] for _ in range(self.n)]
@@ -155,38 +151,40 @@ class NetworkCirculation:
 
         fg = FlowGraph(self.n + 2)
         for edge in self.edges:
-            # print(f"Add edge {edge.start}->{edge.end} capacity={edge.capacity - edge.low_bound}")
             fg.add_edge(edge.start, edge.end, edge.capacity - edge.low_bound)
 
-        # add super-source and super-sink
+        # calculate vertices demands
+        demands = [0] * self.n
         for v in range(self.n):
-            source_capacity = sum([self.edges[i].low_bound for i in adj_list_prev[v]])
-            if source_capacity != 0:
-                fg.add_edge(self.n, v, source_capacity)
-                # print(f"Add edge source->{v} capacity={source_capacity}")
+            demands[v] -= sum([self.edges[i].low_bound for i in adj_list_prev[v]])
+            demands[v] += sum([self.edges[i].low_bound for i in adj_list_next[v]])
 
-            sink_capacity = sum([self.edges[i].low_bound for i in adj_list_next[v]])
-            if sink_capacity != 0:
-                fg.add_edge(v, self.n + 1, sink_capacity)
-                # print(f"Add edge {v}->sink capacity={sink_capacity}")
+        # add super-source and super-sink edges
+        for v in range(self.n):
+            demand = demands[v]
+            if demand < 0:
+                # Add edge source->{v + 1} with capacity={abs(demand)}
+                fg.add_edge(self.n, v, abs(demand))
+            elif demand > 0:
+                # Add edge {v + 1}->sink with capacity={demand}
+                fg.add_edge(v, self.n + 1, demand)
 
-        fg.max_flow(start=self.n, end=self.n + 1)
+        fg.maximize_flow(start=self.n, end=self.n + 1)
 
         # check super-source and super-sink edges are saturated
-        for edge_id in fg.graph[self.n]:
-            edge = fg.edges[edge_id]
-            if edge.flow != edge.capacity:
+        for edge in fg.edges:
+            if (edge.start == self.n) and (edge.flow != edge.capacity):
+                # Super-source not saturated!
                 return None
-        for edge_id in fg.graph[self.n + 1]:
-            edge = fg.edges[edge_id]
-            if edge.flow != edge.capacity:
+            elif (edge.end == self.n + 1) and (edge.flow != edge.capacity):
+                # Super-sink not saturated!
                 return None
 
         # construct solution
         circulation = []
         for i, edge in enumerate(self.edges):
-            print(f"Res flow through {i}: {fg.edges[i * 2].flow}")
-            circulation.append(edge.low_bound + fg.edges[i * 2].flow)
+            edge.flow = edge.low_bound + fg.edges[i * 2].flow
+            circulation.append(edge.flow)
         return circulation
 
     def check_naive_solution(self):
@@ -226,25 +224,17 @@ class NetworkCirculation:
 def run_test():
     n_vertices = 3
     edges_raw = [
-        (1, 2, 0, 3),
-        (2, 3, 0, 3),
-    ]
-    edges_raw = [
         (1, 2, 1, 3),
         (2, 3, 2, 4),
         (3, 1, 1, 2),
     ]
-    # edges_raw = [
-    #     (1, 2, 1, 3),
-    #     (2, 3, 2, 4),
-    #     (1, 3, 1, 2),
-    # ]
     edges = []
     for v1, v2, low, cap in edges_raw:
         edges.append(EdgeNC(v1 - 1, v2 - 1, low, cap))
     edges = tuple(edges)
 
-    circulation = NetworkCirculation(n_vertices, edges).solve()
+    nc = NetworkCirculation(n_vertices, edges)
+    circulation = nc.solve()
 
     if circulation is not None:
         print("YES")
@@ -252,6 +242,36 @@ def run_test():
             print(flow)
     else:
         print("NO")
+
+
+def run_stress_test():
+    i = 0
+    while True:
+        print(i)
+        n_vertices = randint(2, 40)
+        n_edges = randint(1, 1600)
+        edges = []
+        for _ in range(n_edges):
+            v1 = randint(1, n_vertices)
+            v2_choices = list(range(1, n_vertices + 1))
+            v2_choices.remove(v1)
+            v2 = choice(v2_choices)
+            cap = randint(0, 50)
+            low = randint(0, cap)
+            edges.append(EdgeNC(v1 - 1, v2 - 1, low, cap))
+        edges = tuple(edges)
+
+        nc = NetworkCirculation(n_vertices, edges)
+        circulation = nc.solve()
+
+        if (circulation is not None) and (not nc.check_solution(nc.n, nc.edges)):
+            # print(f"n_vertices={n_vertices}")
+            # print(f"n_edges={n_edges}")
+            # for edge in edges:
+            #     print(f"{edge.start + 1}->{edge.end + 1} {edge.low_bound}<={edge.flow}<={edge.capacity}")
+            break
+
+        i += 1
 
 
 def run_algo():
@@ -273,5 +293,6 @@ def run_algo():
 
 
 if __name__ == "__main__":
-    run_test()
-    # run_algo()
+    # run_test()
+    # run_stress_test()
+    run_algo()
